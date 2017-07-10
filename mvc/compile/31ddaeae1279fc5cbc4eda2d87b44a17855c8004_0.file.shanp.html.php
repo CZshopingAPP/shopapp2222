17b1ee9594b84f1355e75ef60694440c1b81a057
mingchenccc <947822283@qq.com>
2017-07-10 15:43:05 +0800

    9999

--- mvc/compile/31ddaeae1279fc5cbc4eda2d87b44a17855c8004_0.file.shanp.html.php
+++ mvc/compile/31ddaeae1279fc5cbc4eda2d87b44a17855c8004_0.file.shanp.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-07-10 05:35:50
+/* Smarty version 3.1.30, created on 2017-07-10 06:02:41
   from "E:\wampserve\wamp\www\shopapp2222\mvc\template\index\shanp.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5962f616eed878_82254168',
+  'unifunc' => 'content_5962fc61ac0421_52035432',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '31ddaeae1279fc5cbc4eda2d87b44a17855c8004' => 
     array (
       0 => 'E:\\wampserve\\wamp\\www\\shopapp2222\\mvc\\template\\index\\shanp.html',
-      1 => 1499657750,
+      1 => 1499659360,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5962f616eed878_82254168 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5962fc61ac0421_52035432 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,16 +34,22 @@ function content_5962f616eed878_82254168 (Smarty_Internal_Template $_smarty_tpl)
  src="<?php echo JS_PATH;?>
 /rem.js"><?php echo '</script'; ?>
 >
+    <?php echo '<script'; ?>
+ src="<?php echo JS_PATH;?>
+/jquery.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="<?php echo JS_PATH;?>
+/swiper.min.js"><?php echo '</script'; ?>
+>
     <style>
         .box{
             width: 100%;height: 13.34rem;
-            position: relative;
+            overflow: hidden;
         }
         .box li{ width: 100%;height: 100%;
-            position: absolute;
-            left: 0;top: 0;
         }
-        .box li:nth-child(1){
+       /* .box li:nth-child(1){
             background: url("<?php echo IMG_PATH;?>
 /sp111 (1).png");
             background-position: center;
@@ -63,18 +69,34 @@ function content_5962f616eed878_82254168 (Smarty_Internal_Template $_smarty_tpl)
             background-position: center;
             background-size: 100%;
             background-repeat: no-repeat;
-        }
+        }*/
         .active{
             z-index: 2;
         }
     </style>
 </head>
 <body>
-<ul class="box">
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
+<div class="hjy-banner swiper-container">
+    <ul class="swiper-wrapper box">
+        <li class="swiper-slide">
+            <img src="<?php echo IMG_PATH;?>
+/sp111 (1).png" alt="">
+        </li>
+        <li class="swiper-slide">
+            <img src="<?php echo IMG_PATH;?>
+/sp111 (2).png" alt="">
+        </li>
+        <li class="swiper-slide">
+            <img src="<?php echo IMG_PATH;?>
+/sp111 (3).png" alt="">
+        </li>
+    </ul>
+</div>
 </body>
+<?php echo '<script'; ?>
+>
+
+<?php echo '</script'; ?>
+>
 </html><?php }
 }
